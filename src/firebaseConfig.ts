623ae325148e62120108495dba247c47fb831bdf
Ashley <adent10@gmail.com>
2022-12-22 16:37:13 -1000

    first commit

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyA9Z-1Gjo8yG_NfkCqlUYU1LF3gST7s4gU",
-  authDomain: "car-db-react-final.firebaseapp.com",
-  projectId: "car-db-react-final",
-  storageBucket: "car-db-react-final.appspot.com",
-  messagingSenderId: "158895569181",
-  appId: "1:158895569181:web:c43c0f67e516a223be89f4"
+  apiKey: "AIzaSyBlBHxe6QYwDXLuGBALXe0eGjAlix4Bihs",
+  authDomain: "car-db-react-3.firebaseapp.com",
+  projectId: "car-db-react-3",
+  storageBucket: "car-db-react-3.appspot.com",
+  messagingSenderId: "808653242343",
+  appId: "1:808653242343:web:25c3fba9dfdaf33f8cc742"
 };
